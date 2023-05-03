af1d7ab36e93c39d79ba0f187dc887a704b5f066
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2023-05-03 22:51:13 +0700

    Add files via upload

---
+++ TestArray2022/TestArray2022/src/account/id083/Main.java
@@ -0,0 +1,38 @@
+package account.id083;
+
+import account.id083.UserAccount;
+
+public class Main {
+    public static void main(String[] args) {
+//        testToString();
+//        testPerson();
+//        testPersonToString();
+        testAddAccount();
+    }
+
+    private static void testToString(){
+        Person p1 = new Person("Sarawit");
+        UserAccount user1 = new UserAccount("BomScoob", p1);
+        System.out.println(user1);
+        System.out.println(p1);
+    }
+
+    private static void testPerson(){
+        Person p1 = new Person("Sarawit");
+    }
+
+    private static void testPersonToString(){
+        Person p1 = new Person("Sarawit");
+        System.out.println(p1);
+    }
+
+    private static void testAddAccount(){
+        Person p1 = new Person("Sarawit");
+        UserAccount user1 = new UserAccount("BomScoob", p1);
+        UserAccount user2 = new UserAccount("BomScoob2", p1);
+        System.out.println("Add account 3:" + p1.addAccount("BomScoob3"));
+        System.out.println("Add account already exists :" + p1.addAccount("BomScoob"));
+
+        System.out.println(p1.toString2());
+    }
+}
